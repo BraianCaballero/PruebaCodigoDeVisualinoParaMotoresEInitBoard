3504067be6629d39f55bddd2f116b0d0a9d52771
Braian Agustín Caballero <83320915+BraianCaballero@users.noreply.github.com>
2023-07-12 13:16:25 -0300

    Update PruebaCodigoDeVisualinoParaMotoresYInitBoard.ino

--- PruebaCodigoDeVisualinoParaMotoresYInitBoard__.ino
+++ PruebaCodigoDeVisualinoParaMotoresYInitBoard__.ino
@@ -32,18 +32,32 @@ void loop() {
 
 void loopDeMotores() {
   while (true) {
-    girarMotores("CLOCKWISE", 255);
+    //girarMotores("CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(5000);
-    girarMotores("STOP", 255);
+    //girarMotores("STOP", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(3000);
-    girarMotores("COUNTER_CLOCKWISE", 255);
+    //girarMotores("COUNTER_CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "COUNTER_CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 255);
     delay(5000);
-    girarMotores("STOP", 255);
+    //girarMotores("STOP", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
+    delay(3000);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 255);
+    delay(5000);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(3000);
   }
 }
 
-void girarMotores(int rotation , int SPEED) {
+/*void girarMotores(int rotation , int SPEED) {
     if (rotation == "CLOCKWISE") {
           digitalWrite(In1, HIGH);
           digitalWrite(In3, LOW);
@@ -65,6 +79,49 @@ void girarMotores(int rotation , int SPEED) {
     analogWrite(EnA, SPEED);
     analogWrite(EnB, SPEED);
     
+}*/
+
+void girarMotor_EnSentido_AVelocidad_(int motor, int sentidoDeGiro, int SPEED) {
+
+  //"motor0" (EnA, In1, In2);
+  //"motor1" (EnB, In3, In4);
+  if (motor == "Motor0") {
+    EnA;
+    In1;
+    In2;
+  } 
+  else if (motor == "Motor1") {
+    EnB;
+    In3;
+    In4;
+  }
+  
+  if (sentidoDeGiro == "CLOCKWISE") {
+    digitalWrite(In1, HIGH);
+    digitalWrite(In3, LOW);
+    digitalWrite(In2, LOW);
+    digitalWrite(In4, HIGH);
+  }
+  else if (sentidoDeGiro == "COUNTER_CLOCKWISE") {
+    digitalWrite(In2, HIGH);
+    digitalWrite(In4, LOW);
+    digitalWrite(In1, LOW);
+    digitalWrite(In3, HIGH);
+  } 
+  else if (sentidoDeGiro == "STOP") {
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, LOW);
+    digitalWrite(In3, LOW);
+    digitalWrite(In4, LOW);
+  }
+    
+  if (EnA) {
+    analogWrite(EnA,SPEED);
+  }
+  
+  if (EnB) {
+    analogWrite(EnB, SPEED);
+  }
 }
 
 

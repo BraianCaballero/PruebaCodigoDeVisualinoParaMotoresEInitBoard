6439e9b3505d85a789fba796841c5d2fcf005ebb
BraianCaballero <braianacaballero20gmail.com>
2023-07-14 10:11:40 -0300

    update

--- PruebaCodigoDeVisualinoParaMotoresYInitBoard__.ino
+++ PruebaCodigoDeVisualinoParaMotoresYInitBoard__.ino
@@ -6,8 +6,13 @@ const int EnA = 3;
 const int EnB = 6;
 const int executeButton = 8;
 
+//Para ambos casos de motores ya sea programacion en dúo o unitaria se van a programar tres direcciones de giro,
+//éstas son en "sentido horario" (giran hacia adelante), sentido "anti horario" (giran hacia atras) y "parar" (para ambos motores).
 
 
+//La función initBoard() inicializa los pines que van conectados al driver de motores de la placa DuinoBot, aunque podria
+//aprovecharse para conectar a cualquier placa, esta debe ir obligatoriamente dentro del "setup" y mismo esta debe ser declarada
+//antes que el "setup" de lo contrario fallaria. 
 void initBoard() {
   Serial.begin(9600);
   pinMode(executeButton, INPUT_PULLUP);
@@ -20,44 +25,58 @@ void initBoard() {
 }
 
 void setup() {  
+  
   initBoard();
   
 }
 
 void loop() {
+  
+  //Para que la placa DuinoBot utilice el boton de ejecución hay que programarle un
+  //"if" con la condición de que si no se presiona el boton asignado al boton de ejecución (pin 8 de Arduino Nano),
+  //no se comience a ejecutar el programa que subimos a la placa,  de lo contario el programa subido iniciaria ni bien se le entregue 
+  //la tension y corriente suficiente a la placa (cabe aclarar que se puede utilizar en cualquier placa pero es obligatorio utilizar esto en la placa DuinoBot).
+  //El "if" con su respectiva condicion deben ir obligatoriamente en el "loop" ya que si se coloca en el "setup" fallaria el boton de ejecución
   if (digitalRead(executeButton) != HIGH) {
     loopDeMotores();  
   }
+  
 }
 
+
 void loopDeMotores() {
+
+  //El "while(true)" está puesto para que su cuerpo se ejecute en bucle de no estar puesto esto solo se ejecutaria una sola vez lo que pongamos en el cuerpo del "loop"
   while (true) {
     //girarMotores("CLOCKWISE", 255);
     girarMotor_EnSentido_AVelocidad_("Motor0", "CLOCKWISE", 255);
-    girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "COUNTER_CLOCKWISE", 200);
     delay(5000);
     //girarMotores("STOP", 255);
     girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
     girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(3000);
     //girarMotores("COUNTER_CLOCKWISE", 255);
-    girarMotor_EnSentido_AVelocidad_("Motor0", "COUNTER_CLOCKWISE", 255);
-    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "COUNTER_CLOCKWISE", 150);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 150);
     delay(5000);
     //girarMotores("STOP", 255);
     girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
     girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(3000);
-    girarMotor_EnSentido_AVelocidad_("Motor0", "CLOCKWISE", 255);
-    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 255);
+    girarMotor_EnSentido_AVelocidad_("Motor0", "CLOCKWISE", 100);
+    girarMotor_EnSentido_AVelocidad_("Motor1", "CLOCKWISE", 100);
     delay(5000);
     girarMotor_EnSentido_AVelocidad_("Motor0", "STOP", 0);
     girarMotor_EnSentido_AVelocidad_("Motor1", "STOP", 0);
     delay(3000);
   }
+  
 }
 
-/*void girarMotores(int rotation , int SPEED) {
+//En este caso se activan ambos motores al mismo tiempo por eso no es necesario asignar valores distintos a los motores
+void girarMotores(int rotation , int SPEED) {
+    
     if (rotation == "CLOCKWISE") {
           digitalWrite(In1, HIGH);
           digitalWrite(In3, LOW);
@@ -79,24 +98,49 @@ void loopDeMotores() {
     analogWrite(EnA, SPEED);
     analogWrite(EnB, SPEED);
     
-}*/
+}
 
+//En este caso es necesario diferenciar los amotores ya que se van a utilizar a programar uno a la vez
 void girarMotor_EnSentido_AVelocidad_(int motor, int sentidoDeGiro, int SPEED) {
-
-  //"motor0" (EnA, In1, In2);
-  //"motor1" (EnB, In3, In4);
+  
+  //MOTOR0 = EnB, In3, In4
   if (motor == "Motor0") {
-    EnA;
-    In1;
-    In2;
+    if (sentidoDeGiro == "CLOCKWISE") {
+    digitalWrite(In3, HIGH);
+    digitalWrite(In4, LOW);
+    }
+    else if (sentidoDeGiro == "COUNTER_CLOCKWISE") {
+      digitalWrite(In4, HIGH);
+      digitalWrite(In3, LOW);
+    } 
+    else if (sentidoDeGiro == "STOP") {
+      digitalWrite(In3, LOW);
+      digitalWrite(In4, LOW);
+    }
+    analogWrite(EnB,SPEED);
   } 
-  else if (motor == "Motor1") {
-    EnB;
-    In3;
-    In4;
+
+  //MOTOR1 = EnA, In1, In2
+  if (motor == "Motor1") {
+    if (sentidoDeGiro == "CLOCKWISE") {
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, HIGH);
+    }
+    else if (sentidoDeGiro == "COUNTER_CLOCKWISE") {
+      digitalWrite(In2, LOW);
+      digitalWrite(In1, HIGH);
+    } 
+    else if (sentidoDeGiro == "STOP") {
+      digitalWrite(In1, LOW);
+      digitalWrite(In2, LOW);
+    }
+    analogWrite(EnA, SPEED);
   }
   
-  if (sentidoDeGiro == "CLOCKWISE") {
+}  
+
+
+  /*if (sentidoDeGiro == "CLOCKWISE") {
     digitalWrite(In1, HIGH);
     digitalWrite(In3, LOW);
     digitalWrite(In2, LOW);
@@ -121,7 +165,4 @@ void girarMotor_EnSentido_AVelocidad_(int motor, int sentidoDeGiro, int SPEED) {
   
   if (EnB) {
     analogWrite(EnB, SPEED);
-  }
-}
-
-
+  }*/
